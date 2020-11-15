4d42a2b0b4adaea9d0f2258a40c37b2371ae6170
544B <2612737+544B@users.noreply.github.com>
2020-11-15 17:41:05 +0900

    step.04: check train data distribution

--- step_04/src/main.py
+++ step_04/src/main.py
@@ -1,7 +1,32 @@
 import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+
+
+OUTPUT_DIR = '/output'
+NUM_WORDS = 10000
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+def multi_hot_sequences(sequences, dimension):
+    # 形状が (len(sequences), dimension)ですべて0の行列を作る
+    results = np.zeros((len(sequences), dimension))
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0  # 特定のインデックスに対してresults[i] を１に設定する
+    return results
+
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
 
 def main():
-    print(tf.__version__)
+    plt.plot(train_data[0])
+    plt.savefig(os.path.join(OUTPUT_DIR, 'check.png'))
+
 
 
 if __name__ == '__main__':
