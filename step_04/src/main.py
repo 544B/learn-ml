dfa04c75c9aa27725f4a48a79a9e0eaac396dbab
544B <2612737+544B@users.noreply.github.com>
2020-11-15 18:11:27 +0900

    step.04: history comparison

--- step_04/src/main.py
+++ step_04/src/main.py
@@ -25,9 +25,10 @@ test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
 
 plt.plot(train_data[0])
 plt.savefig(os.path.join(OUTPUT_DIR, 'check.png'))
+plt.clf()
 
 
-def main():
+def get_baseline_model_history():
     baseline_model = keras.Sequential([
         # `.summary` を見るために`input_shape`が必要 
         keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,)),
@@ -40,6 +41,83 @@ def main():
                         metrics=['accuracy', 'binary_crossentropy'])
 
     baseline_model.summary()
+    return baseline_model.fit(train_data,
+                train_labels,
+                epochs=20,
+                batch_size=512,
+                validation_data=(test_data, test_labels),
+                verbose=2)
+
+
+def get_smaller_model_history():
+    smaller_model = keras.Sequential([
+        keras.layers.Dense(4, activation='relu', input_shape=(NUM_WORDS,)),
+        keras.layers.Dense(4, activation='relu'),
+        keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    smaller_model.compile(optimizer='adam',
+                        loss='binary_crossentropy',
+                        metrics=['accuracy', 'binary_crossentropy'])
+
+    smaller_model.summary()
+    return smaller_model.fit(train_data,
+                train_labels,
+                epochs=20,
+                batch_size=512,
+                validation_data=(test_data, test_labels),
+                verbose=2)
+
+
+def get_bigger_model_history():
+    bigger_model = keras.models.Sequential([
+        keras.layers.Dense(512, activation='relu', input_shape=(NUM_WORDS,)),
+        keras.layers.Dense(512, activation='relu'),
+        keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    bigger_model.compile(optimizer='adam',
+                         loss='binary_crossentropy',
+                         metrics=['accuracy','binary_crossentropy'])
+
+    bigger_model.summary()
+
+    return bigger_model.fit(train_data, train_labels,
+                epochs=20,
+                batch_size=512,
+                validation_data=(test_data, test_labels),
+                verbose=2)
+
+
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16,10))
+
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_'+key],
+                   '--', label=name.title()+' Val')
+
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(),
+                label=name.title()+' Train')
+
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_',' ').title())
+    plt.legend()
+    plt.xlim([0,max(history.epoch)])
+
+    plt.savefig(os.path.join(OUTPUT_DIR, 'history_comparison.png'))
+    plt.clf()
+
+
+def main():
+    baseline_history = get_baseline_model_history()
+    smaller_history = get_smaller_model_history()
+    bigger_history = get_bigger_model_history()
+
+    plot_history([
+        ('baseline', baseline_history),
+        ('smaller', smaller_history),
+        ('bigger', bigger_history)
+    ])
 
 
 if __name__ == '__main__':
