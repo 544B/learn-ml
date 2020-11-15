167299e005f906af80ec4cb49b65a20486d95a53
544B <2612737+544B@users.noreply.github.com>
2020-11-15 10:58:41 +0900

    step.03: decode review

--- step_03/src/main.py
+++ step_03/src/main.py
@@ -10,8 +10,22 @@ imdb = keras.datasets.imdb
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 
 
+def decode_review(text):
+    # 単語を整数にマッピングする辞書
+    word_index = imdb.get_word_index()
+    # インデックスの最初の方は予約済み
+    word_index = {k:(v+3) for k,v in word_index.items()} 
+    word_index["<PAD>"] = 0
+    word_index["<START>"] = 1
+    word_index["<UNK>"] = 2  # unknown
+    word_index["<UNUSED>"] = 3
+    reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
 def main():
-    print(tf.__version__)
+    print(train_data[0])
+    print(decode_review(train_data[0]))
 
 
 if __name__ == '__main__':
